78d8412e13b575e4f22d59c5ffa839fa39db6e1b
JesusAdanOp <jesusadanorozcop@outlook.com>
2022-09-19 08:44:47 -0600

    Agregar archivos de proyecto.

---
+++ Resta Con Constructor/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resta_Con_Constructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            clresta resta = new clresta(int.Parse(txtV1.Text), int.Parse(txtV2.Text));
+            txtResultado.Text = resta.resta().ToString();
+        }
+    }
+}
---
+++ Resta Con Constructor/clresta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resta_Con_Constructor
+{
+    internal class clresta
+    {
+        int v1, v2;
+
+        public clresta(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public int resta()
+        {
+            return v1-v2;
+        }
+    }
+}
